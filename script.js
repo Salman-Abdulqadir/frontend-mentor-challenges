4379e85085624550827327b86d1ae398eaa08395
salman-abdulqadir <salmanabdulkadir82@gmail.com>
2024-12-21 12:12:10 +0400

    init commit

--- script.js
+++ script.js
@@ -1,58 +1,150 @@
-const FAQs = [
-  {
-    question: "What is Frontend Mentor, and how will it help me?",
-    answer: `Frontend Mentor offers realistic coding challenges to help developers improve their frontend
-        coding skills with projects in HTML, CSS, and JavaScript. It's suitable for
-        all levels and ideal for portfolio building.`,
-  },
-  {
-    question: "Is Frontend Mentor free?",
-    answer: `Yes, Frontend Mentor offers both free and premium coding challenges, with the
-        free option providing access to a range of projects suitable for all skill
-        levels.`,
-  },
-  {
-    question: "Can I use Frontend Mentor projects in my portfolio?",
-    answer: `Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
-      way to showcase your skills to potential employers!`,
-  },
-  {
-    question: "How can I get help if I'm stuck on a Frontend Mentor challenge?",
-    answer: `Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
-      way to showcase your skills to potential employers!`,
-  },
-];
+const renderServices = () => {
+  const services = [
+    {
+      title: "Online Banking",
+      desc: `Our modern web and mobile applications allow you to keep track of
+          your finances wherever you are in the world.`,
+      imageSrc: "./images/icon-online.svg",
+    },
+    {
+      title: "Simple Budgeting",
+      desc: `See exactly where your money goes each month. Receive notifications
+      when you’re close to hitting your limits.`,
+      imageSrc: "./images/icon-budgeting.svg",
+    },
+    {
+      title: "Fast Onboarding",
+      desc: `We don’t do branches. Open your account in minutes online and start
+      taking control of your finances right away.`,
+      imageSrc: "./images/icon-onboarding.svg",
+    },
+    {
+      title: "Open API",
+      desc: `Manage your savings, investments, pension, and much more from one
+      account.`,
+      imageSrc: "./images/icon-api.svg",
+    },
+  ];
 
-const faqContainer = document.getElementById("faqs");
+  const servicesContainer = document.getElementById("services-container");
+  if (!servicesContainer) return;
 
-const getFAQAccordion = (question, answer, openByDefault = false) => {
-  const details = document.createElement("details");
-  details.open = openByDefault;
+  const serviceCards = services?.map((service) => {
+    const logo = document.createElement("img");
+    logo.src = service.imageSrc;
+    logo.alt = service.title;
+    const title = document.createElement("h3");
+    title.innerHTML = service.title;
+    const desc = document.createElement("p");
+    desc.innerHTML = service.desc;
 
-  const questionDOM = document.createElement("summary");
-  const icon = document.createElement("img");
-  const iconSrcs = {
-    open: "./images/icon-plus.svg",
-    close: "./images/icon-minus.svg",
-  };
-  icon.src = openByDefault ? iconSrcs.close : iconSrcs.open;
-
-  questionDOM.innerHTML = question;
-  questionDOM.appendChild(icon);
-  questionDOM.addEventListener("click", () => {
-    const isOpen = !details.open;
-    icon.src = isOpen ? iconSrcs.close : iconSrcs.open;
+    const serviceCard = document.createElement("div");
+    serviceCard.className = "service-card flex flex-col align-start gap-16";
+    serviceCard.append(logo, title, desc);
+    return serviceCard;
   });
 
-  const answerDOM = document.createElement("p");
-  answerDOM.innerHTML = answer;
-
-  details.append(questionDOM, answerDOM);
-  return details;
+  servicesContainer.append(...serviceCards);
 };
 
-const faqList = FAQs.map((qa, index) =>
-  getFAQAccordion(qa.question, qa.answer, index === 0)
-);
+const renderArticles = () => {
+  const articles = [
+    {
+      imgSrc: "./images/image-currency.jpg",
+      author: "Claire Robinson",
+      title: "Receive money in any currency with no fees",
+      desc: `The world is getting smaller and we’re becoming more mobile. So why should you be forced to only
+      receive money in a single … `,
+    },
+    {
+      imgSrc: "./images/image-restaurant.jpg",
+      author: "Wilson Hutton",
+      title: "Treat yourself without worrying about money",
+      desc: `Our simple budgeting feature allows you to separate out your
+      spending and set realistic limits each month. That means you … `,
+    },
+    {
+      imgSrc: "./images/image-plane.jpg",
+      author: "Wilson Hutton",
+      title: "Take your Easybank card wherever you go",
+      desc: `We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re
+      abroad. We’ll even show you …  `,
+    },
+    {
+      imgSrc: "./images/image-confetti.jpg",
+      author: "Claire Robinson",
+      title: "Our invite-only Beta accounts are now live!",
+      desc: `After a lot of hard work by the whole team, we’reexcited to launch our closed beta. It’s easy to request an invite through
+      the site ...`,
+    },
+  ];
 
-faqContainer.append(...faqList);
+  const articlesContainer = document.getElementById("articles-container");
+  if (!articlesContainer) return;
+
+  const articleCards = articles?.map((article) => {
+    const cover = document.createElement("img");
+    cover.className = "article-img";
+    cover.src = article.imgSrc;
+    cover.alt = article.imgSrc + article.author;
+
+    const author = document.createElement("h5");
+    author.innerText = `By ${article.author}`;
+
+    const title = document.createElement("h4");
+    title.innerText = article.title;
+
+    const desc = document.createElement("p");
+    desc.innerText = article.desc;
+
+    const detailsContainer = document.createElement("div");
+    detailsContainer.className =
+      "flex-col p-16 align-start article-card-details";
+    detailsContainer.append(author, title, desc);
+
+    const articleCard = document.createElement("div");
+    articleCard.className = "article-card";
+    articleCard.append(cover, detailsContainer);
+
+    return articleCard;
+  });
+
+  articlesContainer.append(...articleCards);
+};
+
+const renderSocialMediaLinks = () => {
+  const socialMediaLinks = [
+    {
+      logo: "./images/icon-facebook.svg",
+      alt: "facebook",
+      link: "https://facebook.com",
+    },
+    {
+      logo: "./images/icon-youtube.svg",
+      alt: "youtube",
+      link: "https://youtube.com",
+    },
+    {
+      logo: "./images/icon-twitter.svg",
+      alt: "twitter",
+      link: "https://twitter.com",
+    },
+    {
+      logo: "./images/icon-pinterest.svg",
+      alt: "pinterest",
+      link: "https://pinterest.com",
+    },
+    {
+      logo: "./images/icon-instagram.svg",
+      alt: "instgram",
+      link: "https://instagram.com",
+    },
+  ];
+};
+
+function main() {
+  renderServices();
+  renderArticles();
+}
+
+main();
